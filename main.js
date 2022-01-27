8f63026dd282534244218400c51e6718b4b07446
step31 <cmille749@gmail.com>
2022-01-27 08:51:52 +0100

    bulle

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 // ----------------------HEADER
 
 const header = document.createElement("div");
+const divLogo = document.createElement("div");
 const logo = document.createElement("img");
+const divBubbles = document.createElement("div");
 const compte = document.createElement("img");
 const panier = document.createElement("img");
 
 header.className = "header";
+divLogo.className = "divLogo";
+logo.className = "logo";
+divBubbles.className = "divBubbles";
 compte.className = "compte";
 panier.className = "panier";
 
 document.body.appendChild(header);
-header.appendChild(logo);
-header.appendChild(compte);
-header.appendChild(panier);
+header.appendChild(divLogo);
+header.appendChild(divBubbles);
+divLogo.appendChild(logo);
+divBubbles.appendChild(compte);
+divBubbles.appendChild(panier);
 
 logo.src = "img/Logo.png";
 compte.src = "img/Compte.png";
@@ -24,8 +31,11 @@ const section1 = document.createElement("section");
 const divLeft1 = document.createElement("div");
 const imgAcc = document.createElement("img");
 
+section1.className = "section1";
+divLeft1.className = "divLeft";
 imgAcc.className = "imgAcc";
 imgAcc.src = "img/Accueil(2).png";
+divLeft1.className = "divLeft1";
 
 document.body.appendChild(section1);
 section1.appendChild(divLeft1);
@@ -33,27 +43,45 @@ divLeft1.appendChild(imgAcc);
 
 // -----------------RIGHT-----------------
 const divRight1 = document.createElement("div");
+const divBubblesR = document.createElement("div");
 const divBubble1 = document.createElement("div");
 const divBubble2 = document.createElement("div");
 const divBubble3 = document.createElement("div");
 const imgBubble1 = document.createElement("img");
 const imgBubble2 = document.createElement("img");
 const imgBubble3 = document.createElement("img");
+const aboutAcc = document.createElement("div");
+const rhumAcc = document.createElement("div");
+const sugAcc = document.createElement("div");
 
+divBubblesR.className = "divBubblesR";
+divBubble1.className = "divBubble1";
+divBubble2.className = "divBubble2";
+divBubble3.className = "divBubble3";
 imgBubble1.className = "imgBubble1";
-imgBubble1.src = "img/image 2.png";
+imgBubble1.src = "img/BubbleBob.png";
 imgBubble2.className = "imgBubble2";
-imgBubble2.src = "img/image 2.png";
+imgBubble2.src = "img/BubbleRhum.png";
 imgBubble3.className = "imgBubble3";
-imgBubble3.src = "img/image 2.png";
+imgBubble3.src = "img/BubbleSug.png";
+aboutAcc.className = "aboutAcc";
+aboutAcc.innerHTML = "Qui sommes-nous ?";
+rhumAcc.className = "rhumAcc";
+rhumAcc.innerHTML = "Découvrez nos rhums";
+sugAcc.className = "sugAcc";
+sugAcc.innerHTML = "Découvrez nos sucres de canne";
 
 section1.appendChild(divRight1);
-divRight1.appendChild(divBubble1);
-divRight1.appendChild(divBubble2);
-divRight1.appendChild(divBubble3);
+divRight1.appendChild(divBubblesR);
+divBubblesR.appendChild(divBubble1);
+divBubblesR.appendChild(divBubble2);
+divBubblesR.appendChild(divBubble3);
 divBubble1.appendChild(imgBubble1);
 divBubble2.appendChild(imgBubble2);
 divBubble3.appendChild(imgBubble3);
+divBubble1.appendChild(aboutAcc);
+divBubble2.appendChild(rhumAcc);
+divBubble3.appendChild(sugAcc);
 
 // SECTION 2
 
@@ -61,8 +89,10 @@ const section2 = document.createElement("section");
 const divLeft2 = document.createElement("div");
 const imgS2 = document.createElement("img");
 
+section2.className = "section2";
+divLeft2.className = "divLeft2";
 imgS2.className = "imgS2";
-imgS2.src = "img/Accueil(2).png";
+imgS2.src = "img/BubbleBob.png";
 
 document.body.appendChild(section2);
 section2.appendChild(divLeft2);
