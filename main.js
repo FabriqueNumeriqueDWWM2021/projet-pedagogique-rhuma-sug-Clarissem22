30bbca19fa00fede570d3e53ebc5800653d5032e
step31 <cmille749@gmail.com>
2022-01-31 16:32:01 +0100

    event

---
+++ main.js
@@ -0,0 +1,6 @@
+const compte = document.querySelectorAll(".compte");
+const panier = document.querySelectorAll(".panier");
+
+compte.addEventListener("click", function () {
+  document.location.href = "http://localhost/Rhuma_sug/compte.php";
+});
