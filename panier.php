261c7f3b988989c4f52be8a8711b13d486e291a3
step31 <cmille749@gmail.com>
2022-01-31 14:36:42 +0100

    panier

--- panier.php
+++ panier.php
@@ -5,13 +5,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon panier</title>
+    <title>Mon compte</title>
     <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
     <?php include "./header.php" ?>
+    <section class="sectionP">
+        <div class="divLeftP">
 
+            <div>
+                <img class="imgPanier" src="img/Buuuble.png">
+                <div class="divProduitS3">
+                    <div>
+                        <h1>MON PANIER</h1>
+                    </div>
+                    <div>
+                        <img class="ajoutP" src="img/ContinuerAchats.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="divRightP">
+            <div class="divBubbleR">
+                <img class="imgPanier" src="img/Buuuble.png">
+                <div class="divProduitS3">
+                    <div>
+                        <h1>MONTANT DU PANIER</h1>
+                    </div>
+                    <div class="RhumAmbre">
+
+                    </div>
+                    <div>
+                        <img class="ajoutP" src="img/ValierPanier (1).png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
 
 </body>
 
