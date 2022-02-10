fe7dd013c248bdab4e297ad984d875589c0ff0b2
step31 <cmille749@gmail.com>
2022-02-10 11:26:27 +0100

    tortue

--- panier.php
+++ panier.php
@@ -10,43 +10,48 @@
 
 </head>
 
-<body>
+<body class="bodyazerty">
     <?php include "template/header.php" ?>
-    <section class="sectionP">
-        <div class="divLeftP">
+    <div class="azerty">
+        <section class="sectionP">
+            <div class="divLeftP">
 
-            <div>
-                <img class="imgPanier" src="img/Buuuble.png">
-                <div class="divProduitS3">
-                    <div>
-                        <h1>MON PANIER</h1>
-                    </div>
-                    <div>
-                        <img class="continuerAchat" src="img/ContinuerAchats.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="divRightP">
-            <div class="divBubbleR">
-                <img class="imgPanier" src="img/Buuuble.png">
-                <div class="divProduitS3">
-                    <div>
-                        <h1>MONTANT DU PANIER</h1>
-                    </div>
-                    <div class="RhumAmbre">
-
-                    </div>
-                    <div>
-                        <img class="ajoutP" src="img/ValierPanier (1).png" alt="">
+                <div>
+                    <img class="imgPanier" src="img/Buuuble.png">
+                    <div class="divProduitS3">
+                        <div>
+                            <h1>MON PANIER</h1>
+                        </div>
+                        <div>
+                            <img class="continuerAchat" src="img/ContinuerAchats.png" alt="">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div class="divRightP">
+                <div class="divBubbleR">
+                    <img class="imgPanier" src="img/Buuuble.png">
+                    <div class="divProduitS3">
+                        <div>
+                            <h1>MONTANT DU PANIER</h1>
+                        </div>
+                        <div class="RhumAmbre">
+
+                        </div>
+                        <div>
+                            <img class="ajoutP" src="img/ValierPanier (1).png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
-    </section>
+        </section>
+    </div>
+    <div class="azerty">
+        <img class="imgTortuep" src="img/bulleTortue.png" alt="">
+    </div>
     <script src="js/main.js"> </script>
     <script type="module" src="./js/insererProduit.js"> </script>
     <script type="module" src="./js/demo.js"> </script>
